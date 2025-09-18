777cb45e20beb32c8a09b0e39a660267661e24fd
BMoradi1 <93304660+BMoradi1@users.noreply.github.com>
2025-09-17 17:29:20 -0700

    final stuff

---
+++ assign1/Submitted100%/pcbtable.cpp
@@ -0,0 +1,69 @@
+/**
+ * Assignment 1: priority queue of processes
+ * @file pcbtable.h
+ * @author Bijan Moradi, Bryn Grofcsik 
+ * @brief This is the implementation file for the PCBTable class.
+ * //You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+ * // Remember to add sufficient comments to your code
+ */
+
+#include "pcbtable.h"
+#include <vector>
+
+/**
+ * @brief Construct a new PCBTable object of the given size (number of PCBs)
+ *
+ * @param size: the capacity of the PCBTable
+ */
+PCBTable::PCBTable(int size) 
+{
+    
+   // TODO: add your code here
+ // std::vector<PCB*> table; //allocate our vector of PCB pointers
+  table.resize(size);
+}
+
+/**
+ * @brief Destroy the PCBTable object. Make sure to delete all the PCBs in the table.
+ *
+ */
+PCBTable::~PCBTable() 
+{
+   // TODO: add your code here
+   // Delete all the PCBs in the table
+   while(!table.empty())
+   {
+        delete table.back(); //delete the object at the back of the table vector
+        table.pop_back(); //remove the pointer from the table vectro
+   }
+   table.clear(); //deallocates the table
+}
+
+/**
+ * @brief Get the PCB at index "idx" of the PCBTable.
+ *
+ * @param idx: the index of the PCB to get
+ * @return PCB*: pointer to the PCB at index "idx"
+ */
+PCB* PCBTable::getPCB(unsigned int idx) 
+{
+    
+    return table[idx];
+}
+
+/**
+ * @brief Add a PCB pointer to the PCBTable at index idx.
+ *
+ * @param pcb: the PCB to add
+ */
+void PCBTable::addPCB(PCB *pcb, unsigned int idx) {
+    // TODO: add your code here
+    // Add a PCB pointer to the PCBTable at index idx.
+    
+    if(idx > table.size())
+    {
+        idx = table.size();
+    }
+    table.insert(table.begin() + idx, pcb);
+    //cout << "Inserted PCB with priority value:" << pcb -> getPriority()<< " into index: "<< idx <<endl;
+ }
