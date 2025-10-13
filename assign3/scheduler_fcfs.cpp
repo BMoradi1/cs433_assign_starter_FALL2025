82dde91004ccf4711dda214349e121a54421aff6
grobry <bgrofcsik@gmail.com>
2025-10-12 20:02:14 -0700

    Started FCFS

--- assign3/scheduler_fcfs.cpp
+++ assign3/scheduler_fcfs.cpp
@@ -10,5 +10,43 @@
 
 #include "scheduler_fcfs.h"
 
+vector<int> waitTimes;
+vector<int> turnaroundTimes;
+vector<PCB> FCFSschedule;
+
 // TODO: add implementation of SchedulerFCFS constructor, destrcutor and 
+SchedulerFCFS::SchedulerFCFS(){
+
+}
+
+SchedulerFCFS::~SchedulerFCFS(){
+}
+
+void init(std::vector<PCB>& process_list){
+//Doesnt need to be sorted since its FCFS
+int time = 0;
+int wait = 0;
+for (int i = 0; i < process_list.size(); i++){
+cout << "Running Process " << process_list[i].name << " for " << process_list[i].burst_time << " time units" << endl;
+
+FCFSschedule[i] = process_list[i]; //accessibility for this class
+
+wait = wait + time;
+waitTimes[i] = wait;
+time = time + process_list[i].burst_time;
+turnaroundTimes[i] = time;
+
+   }
+}
+
+void print_results(){
+
+for (int i = 0; i < FCFSschedule.size(); i++){
+   cout << FCFSschedule[i].name << " turnaround time = " << turnaroundTimes[i] << ", waiting time = " << waitTimes[i] << endl;
+   }
+}
+
+void simulate(){
+cout << "waht " << endl;
+}
 // member functions init, print_results, and simulate here
