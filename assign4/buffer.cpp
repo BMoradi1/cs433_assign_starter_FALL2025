e1e592be18208dab5d793f5fc5ff3ba9980a1d66
BMoradi1 <93304660+BMoradi1@users.noreply.github.com>
2025-11-09 12:49:28 -0800

    fixed formatting. Added additional logic
    Untested

--- assign4/buffer.cpp
+++ assign4/buffer.cpp
@@ -15,72 +15,96 @@
  /* 5. Sleep */
  /* 6. Exit */
 
- #include "buffer.h"
- #include <iostream>
- using namespace std;
+#include "buffer.h"
+#include <iostream>
+//using namespace std;
 
 
 //Buffer Object and Global Variables
- buffer_item OurBuffer[0];
- int bufferCount = 0;
- int bufferSize = 0;
+buffer_item **OurBuffer = new buffer_item*[0];
+int bufferCount = 0;
+int bufferSize = 0;
 
  //Constructor
- Buffer::Buffer(int size){
- buffer_item OurBuffer[size];
- bufferSize = size;
- }
+Buffer::Buffer(int size)
+{
+   buffer_item* OurBuffer = new buffer_item[size];
+   bufferSize = size;
+}
 
- //Deconstructor
- Buffer::~Buffer(){
- }
+//Deconstructor
+Buffer::~Buffer()
+{
+   for(int i = 0; i < bufferSize;i++)
+   {
+      delete OurBuffer[i];
+   }
+   delete OurBuffer;
+}
 
- //Inserts an item into the buffer, bool = success 
- bool insert_item(buffer_item item){
- bool success;
- }
-//Removes an item from the buffer, bool = success 
- bool remove_item(buffer_item *item){
- bool success;
- }
+//Inserts an item into the buffer, On successful insertion return true.
+bool insert_item(buffer_item *item)
+{
+   if(!is_full())
+   {
+      OurBuffer[bufferCount] = item;
+      bufferCount++;
+      return true;
+   }
+   return false;
+}
+//Removes an item from the buffer, On successful insertion return true.
+bool remove_item(buffer_item *item)
+{
+   if(is_empty())
+   {}
+}
 
- //Returns size of the buffer; how many items it can hold
- int get_size(){
+//Returns size of the buffer; how many items it can hold
+int get_size()
+{
  return bufferSize;
- }
+}
 
- //Returns the count of the number of items currently in buffer
- int get_count(){
- return bufferCount;
- }
+//Returns the count of the number of items currently in buffer
+int get_count()
+{
+   return bufferCount;
+}
 
- //Checks if buffer is empty
- bool is_empty(){
- if(bufferCount <= 0){
-    return true;
-    }
- return false;
- }
-
- //Checks if buffer is full
- bool is_full(){
- if(bufferCount >= bufferSize){
-    return true;
-    }
+//Checks if buffer is empty
+bool is_empty()
+{
+   if(bufferCount <= 0)
+   {
+      return true;
+   }
 return false;
- }
+}
 
- //Prints a list of items in the buffer
- void print_buffer(){
-    string arrayPrint;
-    arrayPrint.append("Buffer: [");
-    for(int i = 0; i < bufferCount; i++){
-       arrayPrint.append(to_string(OurBuffer[i]));
-       arrayPrint.append(", ");
-    }
-    arrayPrint.erase(arrayPrint.size() - 2); //removes the last comma and space
-    arrayPrint.append("]");
- }
+//Checks if buffer is full
+bool is_full()
+{
+   if(bufferCount >= bufferSize)
+   {
+      return true;
+   }
+return false;
+}
+
+//Prints a list of items in the buffer
+void print_buffer()
+{
+   std::string arrayPrint;
+   arrayPrint.append("Buffer: [");
+   for(int i = 0; i < bufferCount; i++)
+   {
+      arrayPrint.append(std::to_string(OurBuffer[i]));
+      arrayPrint.append(", ");
+   }
+   arrayPrint.erase(arrayPrint.size() - 2); //removes the last comma and space
+   arrayPrint.append("]");
+}
 // You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
 
