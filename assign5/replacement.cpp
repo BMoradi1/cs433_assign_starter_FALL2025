da17d0803498c556ced199087c00d417a1c4b953
BMoradi1 <93304660+BMoradi1@users.noreply.github.com>
2025-11-30 12:36:29 -0800

    Update replacement.cpp

--- assign5/replacement.cpp
+++ assign5/replacement.cpp
@@ -34,12 +34,18 @@ bool Replacement::access_page(int page_num, bool is_write)
     // TODO: Add your implementation here
 
     // If the page is valid, it calls the touch_page function. 
-    if() 
+    if(page_table[page_num].valid == true) 
     {
-        touch_page(page_num)
+        touch_page(page_num);
     }
     // If the page is not valid but free frames are available, it calls the load_page function.
     // If the page is not valid and there is no free frame, it calls the replace_page function.
+    if(page_table[page_num].valid == false && ??)
+    (
+        replace_page(page_num);
+        numOfPageReplacements++;
+
+    )
     return false;
 }
 
