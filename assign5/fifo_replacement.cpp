52dbdf349f9330bbe9e1fa409c196fd018bb3d30
grobry <bgrofcsik@gmail.com>
2025-12-03 10:25:13 -0800

    merge test

--- assign5/fifo_replacement.cpp
+++ assign5/fifo_replacement.cpp
@@ -9,17 +9,20 @@
 // Remember to add sufficient and clear comments to your code
 
 #include "fifo_replacement.h"
-int lastReplacedIndex = 0;
 //total page count
 int pageCount;
 //total frame count (size of  table)
 int frameCount;
+int totalFrames;
 
 
 // TODO: Add your implementation here
 FIFOReplacement::FIFOReplacement(int num_pages, int num_frames)
 : Replacement(num_pages, num_frames)
 {
+    pageCount = num_pages;
+    totalFrames = num_frames;
+    frameCount = 0;
     // TODO: Add additional implementation code
     frameCount = 0;
 }
@@ -32,11 +35,12 @@ FIFOReplacement::~FIFOReplacement() {
 // Access an invalid page, but free frames are available
 void FIFOReplacement::load_page(int page_num) {
     // TODO: Update your data structure FIFO replacement and pagetable
-    PageEntry newPage;
+    PageEntry newPage = PageEntry();
 
     newPage.dirty = true;
     newPage.valid = false;
     newPage.frame_num = frameCount;
+
     page_table[frameCount] = newPage;
     frameCount++;
 }
@@ -49,18 +53,13 @@ int FIFOReplacement::replace_page(int page_num) {
 
     newPage.dirty = true;
     newPage.valid = false;
-    newPage.frame_num = page_num;
-
-    page_table[0] = newPage;
+    newPage.frame_num = 0;
 
     for(int i = 0; i < frameCount - 1; i++){
         page_table[i] = page_table[i + 1];
     }
-    if (lastReplacedIndex == frameCount - 1){
-        lastReplacedIndex = 0;
-    }
-    else{
-        lastReplacedIndex++;
-    }
-    
+
+    page_table[frameCount - 1] = newPage;
+
+    return firstIn.frame_num;
 }
