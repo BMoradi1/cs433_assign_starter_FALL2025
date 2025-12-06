d0b4ad51174e6c64de1368bf3c02aec5cd421769
BMoradi1 <93304660+BMoradi1@users.noreply.github.com>
2025-12-05 18:09:38 -0800

    Update replacement.cpp

--- assign5/replacement.cpp
+++ assign5/replacement.cpp
@@ -45,7 +45,8 @@ bool Replacement::access_page(int page_num, bool is_write)
         return false;
     }
     // If the page is not valid but free frames are available, it calls the load_page function.
-    if(page_table[page_num].valid == false && free_frames > 0){
+    if(page_table[page_num].valid == false && free_frames > 0)
+    {
         load_page(page_num);
 
         page_table[page_num].frame_num = current_frame; //page loaded into frame and its now a valid page
